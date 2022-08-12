22966adad6a91dc92b3c3836ee667548818a99d7
Mutammim <58490040+mutammim@users.noreply.github.com>
2022-08-11 20:54:42 -0400

    ✨ Add scenes and levels

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import kaboom from "kaboom";
+import kaboom, { GameObj } from "kaboom";
 
-/* ---------------------------------- Setup --------------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                                    Setup                                   */
+/* -------------------------------------------------------------------------- */
 
 const k = kaboom({
   width: 500,
@@ -22,83 +24,225 @@ loadSprite(SPRITES.player, "https://kaboomjs.com/sprites/bean.png");
 loadSprite(SPRITES.ghost, "https://kaboomjs.com/sprites/ghosty.png");
 loadSprite(SPRITES.watermelon, "https://kaboomjs.com/sprites/watermelon.png");
 
-/* ---------------------------------- Score --------------------------------- */
+/* --------------------------------- Levels --------------------------------- */
 
-let score = 0;
+const LEVELS = [
+  [
+    "w     w   ",
+    "   g      ",
+    "          ",
+    "      w   ",
+    "          ",
+    "          ",
+    "   w      ",
+    "          ",
+    "        gw",
+    "w         ",
+  ],
+  [
+    "w     w   ",
+    "   g    w ",
+    "          ",
+    "      w   ",
+    " g  g     ",
+    "          ",
+    "   w      ",
+    "     w    ",
+    "        gw",
+    "w         ",
+  ],
+];
 
-const scoreText = add([text(score.toString()), pos(24, 24), fixed()]);
-
-function updateScore(newScore: number) {
-  score = newScore;
-  scoreText.text = score.toString();
-}
-
-/* --------------------------------- Player --------------------------------- */
-
-const player = add([sprite(SPRITES.player), pos(100, 100), area()]);
-
-/* ----------------------------- Player movement ---------------------------- */
-
-onKeyDown("left", () => {
-  player.move(-SPEED, 0);
-});
-
-onKeyDown("right", () => {
-  player.move(SPEED, 0);
-});
-
-onKeyDown("up", () => {
-  player.move(0, -SPEED);
-});
-
-onKeyDown("down", () => {
-  player.move(0, SPEED);
-});
-
-/* -------------------------- Generate watermelons -------------------------- */
-
-for (let i = 0; i < 7; i++) {
-  add([
-    sprite(SPRITES.watermelon),
-    pos(Math.random() * 400, Math.random() * 400),
-    area(),
-    "watermelon",
-  ]);
-}
-
-/* ----------------------- Collision with watermelons ----------------------- */
-
-player.onCollide(SPRITES.watermelon, (watermelon) => {
-  destroy(watermelon);
-  updateScore(score + 1);
-});
-
-/* ----------------------------- Generate ghosts ---------------------------- */
-
-for (let i = 0; i < 2; i++) {
-  add([
+const levelConfiguration = {
+  width: 50,
+  height: 50,
+  pos: vec2(0, 0),
+  w: () => [sprite(SPRITES.watermelon), area(), z(0), "watermelon"],
+  g: () => [
     sprite(SPRITES.ghost),
-    pos(200 * (i + 1), Math.random() * 500),
     area(),
-    "ghost",
     state("down", ["up", "down"]),
+    z(1),
+    "ghost",
+  ],
+  p: () => [sprite(SPRITES.player), area(), z(2), "player"],
+};
+
+/* -------------------------------------------------------------------------- */
+/*                                 Start scene                                */
+/* -------------------------------------------------------------------------- */
+
+scene("start", () => {
+  add([
+    text("Press space to start!", {
+      size: 36,
+    }),
+    pos(center()),
+    k.origin("center"),
+    color(0, 255, 255),
   ]);
-}
 
-/* ----------------------------- Ghost movement ----------------------------- */
+  add([
+    text(
+      "Move with WASD or arrow keys. Avoid the ghosts and eat the watermelons to win!",
+      {
+        size: 24,
+        width: 400,
+      }
+    ),
+    pos(center().add(0, 100)),
+    k.origin("center"),
+    color(100, 100, 200),
+  ]);
 
-onUpdate("ghost", (ghost) => {
-  if (ghost.pos.y >= height() - ghost.height) ghost.enterState("up");
-
-  if (ghost.pos.y <= 0) ghost.enterState("down");
-
-  if (ghost.state === "up") ghost.move(0, -SPEED);
-
-  if (ghost.state === "down") ghost.move(0, SPEED);
+  onKeyRelease("space", () => {
+    go("game");
+  });
 });
 
-/* ----------------------------- Ghost collision ---------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                                 Game scene                                 */
+/* -------------------------------------------------------------------------- */
 
-player.onCollide("ghost", () => {
-  destroy(player);
+scene("game", (levelNumber = 0) => {
+  const level = addLevel(LEVELS[levelNumber], levelConfiguration);
+  const player = level.spawn("p", 0, 0);
+
+  /* ---------------------------------- Score --------------------------------- */
+
+  let score = 0;
+
+  const scoreText = add([text(score.toString()), pos(24, 24), fixed(), z(3)]);
+
+  function updateScore(newScore: number) {
+    score = newScore;
+    scoreText.text = score.toString();
+  }
+
+  /* ------------------------------- Level count ------------------------------ */
+
+  add([
+    text(`Level ${levelNumber + 1}/2`, {
+      size: 24,
+    }),
+    pos(360, 20),
+    fixed(),
+    z(3),
+  ]);
+
+  /* ----------------------------- Player movement ---------------------------- */
+
+  onKeyDown(["left", "a"], () => {
+    player.pos.x > 0 ? player.move(-SPEED, 0) : null;
+  });
+
+  onKeyDown(["right", "d"], () => {
+    player.pos.x < 435 ? player.move(SPEED, 0) : null;
+  });
+
+  onKeyDown(["up", "w"], () => {
+    player.pos.y > 0 ? player.move(0, -SPEED) : null;
+  });
+
+  onKeyDown(["down", "s"], () => {
+    player.pos.y < 440 ? player.move(0, SPEED) : null;
+  });
+
+  /* ----------------------- Collision with watermelons ----------------------- */
+
+  player.onCollide(SPRITES.watermelon, (watermelon: GameObj<any>) => {
+    destroy(watermelon);
+    updateScore(score + 1);
+  });
+
+  /* ----------------------------- Ghost movement ----------------------------- */
+
+  onUpdate("ghost", (ghost) => {
+    if (ghost.pos.y >= height() - ghost.height) ghost.enterState("up");
+
+    if (ghost.pos.y <= 0) ghost.enterState("down");
+
+    if (ghost.state === "up") ghost.move(0, -SPEED);
+
+    if (ghost.state === "down") ghost.move(0, SPEED);
+  });
+
+  /* ----------------------------- Ghost collision ---------------------------- */
+
+  player.onCollide("ghost", () => {
+    destroy(player);
+    go("lose");
+  });
+
+  /* ------------------------------ Win scenario ------------------------------ */
+
+  onUpdate(() => {
+    if (get(SPRITES.watermelon).length === 0) {
+      levelNumber < 1 ? go("game", levelNumber + 1) : go("win");
+    }
+  });
 });
+
+/* -------------------------------------------------------------------------- */
+/*                                  Win scene                                 */
+/* -------------------------------------------------------------------------- */
+
+scene("win", () => {
+  add([
+    text("You win :D", {
+      size: 36,
+    }),
+    pos(center()),
+    k.origin("center"),
+    color(0, 255, 255),
+  ]);
+
+  add([
+    text("Press space to restart!", {
+      size: 24,
+      width: 400,
+    }),
+    pos(center().add(0, 100)),
+    k.origin("center"),
+    color(100, 100, 200),
+  ]);
+
+  onKeyRelease("space", () => {
+    go("game", 0);
+  });
+});
+
+/* -------------------------------------------------------------------------- */
+/*                                 Lose scene                                 */
+/* -------------------------------------------------------------------------- */
+
+scene("lose", () => {
+  add([
+    text("You lose :(", {
+      size: 36,
+    }),
+    pos(center()),
+    k.origin("center"),
+    color(0, 255, 255),
+  ]);
+
+  add([
+    text("Press space to restart!", {
+      size: 24,
+      width: 400,
+    }),
+    pos(center().add(0, 100)),
+    k.origin("center"),
+    color(100, 100, 200),
+  ]);
+
+  onKeyRelease("space", () => {
+    go("game", 0);
+  });
+});
+
+/* -------------------------------------------------------------------------- */
+/*                                 Start game                                 */
+/* -------------------------------------------------------------------------- */
+
+go("start");
